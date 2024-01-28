7ad8e0e317172ec0aa4c876c1423bdaecb7418ac
BenjaminJacobson <ben.jacobson@flatironschool.com>
2024-01-28 16:05:30 -0500

    wip

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
     
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, title):
+        if isnstance(title,str) and len(title)>0:
+            self._title = title
+        else:
+            raise ValueError("Title must be string  more than 1 character." )
+
         
