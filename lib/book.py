9010a9887903b55f3f8797feb4b2caa2b9295d55
BenjaminJacobson <ben.jacobson@flatironschool.com>
2024-01-29 21:30:49 -0500

    completed lab

--- lib/book.py
+++ lib/book.py
@@ -12,9 +12,22 @@ class Book:
     
     @title.setter
     def title(self, title):
-        if isnstance(title,str) and len(title)>0:
+        if isinstance(title,str) and len(title)>0:
             self._title = title
         else:
             raise ValueError("Title must be string  more than 1 character." )
 
-        
+    @property
+    def page_count(self):
+        return self._page_count
+    
+    @page_count.setter
+    def page_count(self, page_count):
+        if isinstance(page_count, int):
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+    
+    def turn_page(self):
+        #c = self.page_count + 1
+        print("Flipping the page...wow, you read fast!")
